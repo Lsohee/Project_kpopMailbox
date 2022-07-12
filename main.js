f608064b2542c50d6bbfe4f07ed954aa04829bcd
limsohee <limsohee0521@gmail.com>
2022-07-12 14:53:20 +0900

    #1 task3 알맞는 input 태그 속성 찾기

--- main.js
+++ main.js
@@ -1,13 +1,7 @@
-// console.log(Array.isArray(recipient))
-// console.log(Array.isArray(recipientArr))
-// console.log(submitBtn)
-// console.log(writerId)
-
 const writerId = document.getElementById("writerId")
 const recipient = document.getElementsByName("idle")
 const writingText = document.getElementById("writingText")
 const submitBtn = document.getElementById("submit")
-
 const result = document.getElementById("result")
 const writtenDate =document.getElementById("writtenDate");
 
@@ -17,3 +11,13 @@ memo 나머지 값 (text, date)는 event.target.value로 확인하면 ok
 submitBtn.addEventListener("click",()=>{
   result.innerHTML = writtenDate.value
 })
+
+/*
+pseudo 필요한 정보의 값을 받아드리는 input 태그의 종류
+--> id : text
+--> writtenDate : date
+--> writingText : textarea
+
+
+*/ 
+
